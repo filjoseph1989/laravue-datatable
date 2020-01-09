abcaa71d6e4cb4fba8d951d4367ca6c5346fe23c
Fil <filjoseph22@gmail.com>
2020-01-09 15:56:07 +0800

    Removed unnecessary and minified js and css

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,9 +6,6 @@ import UserDataTableComponent from './components/UserDataTable.vue';
 
 Vue.component('user-data-table', UserDataTableComponent);
 
-import swal from 'sweetalert2';
-window.swal = swal;
-
 var app = new Vue({
     el: '#app'
 });
